89411afc134f2b1200f41a4eacce7e85ec3b4ef8
Caroux Matthias <o22204986@or-iut-i206-f13.iut45.univ-orleans.fr>
2025-01-31 15:35:16 +0100

    ajout de quelque blocs

--- _inc/templates/home.php
+++ _inc/templates/home.php
@@ -3,6 +3,41 @@ $cssPath = "_inc/static/";
 ?>
 
 <main class="home">
-    <input type="text" placeholder="Rechercher un restaurant">
-    <a href="google.fr"><img src="<?php echo $cssPath; ?>loupe.png" alt="logo loupe" id="loupe-image"></a>
-</main>
+    <div class="search-container">
+      <input type="text" placeholder="Rechercher un restaurant, un hôtel..." />
+      <button>
+        <img src="<?php echo $cssPath; ?>loupe.png" alt="Logo">
+      </button>
+    </div>
+    <p class="search-info">
+      Trouvez des restaurants, hôtels et bien plus encore, près de chez vous ou n'importe où dans le monde.
+    </p>
+    <section>
+        <div class="restaurant-container">
+            <div class="restaurant">
+                <div class="restaurant-info">
+                    <h2>Restaurant 1</h2>
+                    <p>Adresse 1</p>
+                    <p>Horaires 1</p>
+                    <p>Note 1</p>
+                </div>
+            </div>
+            <div class="restaurant">
+                <div class="restaurant-info">
+                    <h2>Restaurant 2</h2>
+                    <p>Adresse 2</p>
+                    <p>Horaires 2</p>
+                    <p>Note 2</p>   
+                </div>
+            </div>
+            <div class="restaurant">
+                <div class="restaurant-info">
+                    <h2>Restaurant 3</h2>
+                    <p>Adresse 3</p>
+                    <p>Horaires 3</p>
+                    <p>Note 3</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  </main>
