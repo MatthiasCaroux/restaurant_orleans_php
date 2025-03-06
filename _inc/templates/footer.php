fdb549019b82d070320202cba472ab90a6bb0d73
raphael-cochet <raphael.cochet1234@outlook.fr>
2025-03-06 11:31:07 +0100

    nettoyage de develop

--- _inc/templates/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date("Y"); ?> QuizMaster. Tous droits réservés.</p>
-        </div>
-    </footer>
-
-</body>
-</html>
